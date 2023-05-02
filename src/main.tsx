683a1d2f6b44d0c42c78bd9b6a9fda21fed635bf
CESAR GRACIA <cesar.gracia@pucese.edu.ec>
2023-05-02 12:44:02 -0500

    correrciones

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import './components/view/home'
-import Home from './components/view/home'
+import './components/view/Home'
+import Home from './components/view/Home'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
